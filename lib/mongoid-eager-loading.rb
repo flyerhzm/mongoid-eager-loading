3b5ee85080684af22f2edd8f7364a34ede371fa6
Richard Huang <flyerhzm@gmail.com>
2011-01-20 11:29:05 +0800

    remove unused codes

--- lib/mongoid-eager-loading.rb
+++ lib/mongoid-eager-loading.rb
@@ -1,8 +1,3 @@
 require 'mongoid-eager-loading/mongoid/criterion/eager_loading'
 require 'mongoid-eager-loading/mongoid/criteria'
 require 'mongoid-eager-loading/mongoid/finders'
-
-module Mongoid
-  module EagerLoading
-  end
-end
